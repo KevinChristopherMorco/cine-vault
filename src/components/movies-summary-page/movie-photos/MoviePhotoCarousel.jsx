cc8d76471d1458cf063270f060c4d62bd17a6763
Kevin Christopher Morco <christophermorco02@gmail.com>
2024-10-21 23:52:13 +0800

    adjust gallery

--- src/components/movies-summary-page/movie-photos/MoviePhotoCarousel.jsx
+++ src/components/movies-summary-page/movie-photos/MoviePhotoCarousel.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import IconX from "@tabler/icons-react/dist/esm/icons/IconX.mjs";
 import IconGridDots from "@tabler/icons-react/dist/esm/icons/IconGridDots.mjs";
 import IconChevronLeft from "@tabler/icons-react/dist/esm/icons/IconChevronLeft.mjs";
@@ -11,7 +11,7 @@ import useImageCarousel from "../../../hooks/shared/useImageCarousel";
 const MoviePhotoCarousel = ({ movieData, isLoading }) => {
   const navigate = useNavigate();
 
-  const { photoPath } = useParams();
+  const { movieID, photoPath } = useParams();
 
   const { imageCount, handleImageCount, handleFindImage } = useImageCount();
 
@@ -29,13 +29,13 @@ const MoviePhotoCarousel = ({ movieData, isLoading }) => {
   return (
     <div className="flex h-full flex-col gap-4">
       <div className="flex items-center justify-between px-2">
-        <div
+        <Link
+          to={`/movies-summary/${movieID}`}
           className="flex w-fit items-center gap-1"
-          onClick={() => navigate(-1)}
         >
           <IconX className="h-4 w-4" />
           <p className="text-sm font-medium">Close</p>
-        </div>
+        </Link>
         <div className="flex items-center gap-4">
           <p className="text-sm">
             {imageCount + 1} of {images.length}
@@ -48,7 +48,7 @@ const MoviePhotoCarousel = ({ movieData, isLoading }) => {
           {imageCount > 0 && (
             <IconChevronLeft
               id={"arrow-desc"}
-              className="absolute left-2 top-[50%] -translate-y-[50%] cursor-pointer"
+              className="absolute left-2 top-[50%] h-10 w-10 -translate-y-[50%] cursor-pointer lg:h-14 lg:w-14"
               onClick={handleImageCount}
             />
           )}
@@ -56,12 +56,12 @@ const MoviePhotoCarousel = ({ movieData, isLoading }) => {
           {imageCount + 1 < images.length && (
             <IconChevronRight
               id={"arrow-asc"}
-              className="absolute right-2 top-[50%] -translate-y-[50%] cursor-pointer"
+              className="absolute right-2 top-[50%] h-10 w-10 -translate-y-[50%] cursor-pointer lg:h-14 lg:w-14"
               onClick={handleImageCount}
             />
           )}
         </div>
-        <div className="flex h-[35rem] w-full items-center">
+        <div className="flex h-[35rem] w-full items-center lg:h-[40rem]">
           <img
             src={`http://image.tmdb.org/t/p/w500/${photoPath}`}
             alt=""
--- src/components/movies-summary-page/movie-photos/MoviePhotoDescription.jsx
+++ src/components/movies-summary-page/movie-photos/MoviePhotoDescription.jsx
@@ -1,25 +1,69 @@
 import React from "react";
+import getCastCrew from "../../../helpers/movie/getCastCrew";
+import formatYear from "../../../helpers/format/formatYear";
 
 const MoviePhotoDescription = ({ movieData }) => {
-  const { title } = movieData;
+  const { title, credits, release_date } = movieData;
+  console.log(movieData);
+
+  const { acting: actor, director, writer } = getCastCrew(credits);
+  console.log(actor);
   return (
-    <div className="flex w-full flex-col gap-4 bg-[var(--bg-neutral)] px-2 py-4 text-sm">
-      <div className="">
-        <p>{title}</p>
+    <div className="flex w-full flex-col gap-4 bg-[var(--bg-neutral)] px-2 py-4">
+      <div className="flex items-center gap-2">
+        <p className="text-base font-medium">{title}</p>
+        <p className="text-[.75rem]">({formatYear(release_date)})</p>
       </div>
       <div className="w-full border-t border-gray-500"></div>
       <div className="my-4 grid grid-cols-[1fr_2fr] gap-y-4">
-        <div>Directed By:</div>
-        <div className="flex flex-wrap gap-2">
-          <p>Peter Jackson</p>
-          <p>Peter Jackson</p>
-          <p>Peter Jackson</p>
-          <p>Peter Jackson</p>
-          <p>Peter Jackson</p>
-        </div>
-        <div>Starring:</div>
-        <div className="flex flex-wrap gap-2">
-          <p>Peter Jackson</p>
+        <div className="col-span-2 flex flex-col gap-5 text-sm">
+          {director && director.length > 0 && (
+            <div className="flex">
+              <p className="w-[30%] font-bold md:w-[15%]">
+                {director.length > 1 ? "Directors:" : "Director:"}
+              </p>
+              <ul className="flex w-full flex-wrap gap-2">
+                {director.map((crew, index) => (
+                  <li key={index}>
+                    {crew.name}{" "}
+                    {index !== director.length - 1 && <span>,</span>}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {writer && writer.length > 0 && (
+            <div className="flex">
+              <p className="w-[30%] font-bold md:w-[15%]">
+                {writer.length > 1 ? "Writers:" : "Writer:"}
+              </p>
+              <ul className="flex w-full flex-wrap gap-2">
+                {writer.map((crew, index) => (
+                  <li key={index}>
+                    {crew.name}
+                    {index !== writer.length - 1 && <span>,</span>}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {actor && actor.length > 0 && (
+            <div className="flex">
+              <p className="w-[30%] font-bold md:w-[15%]">
+                {actor.length > 1 ? "Stars:" : "Star:"}
+              </p>
+              <ul className="flex w-full flex-wrap gap-2">
+                {actor.slice(0, 10).map((cast, index) => (
+                  <li key={index}>
+                    {cast.name}
+                    {index < 9 && <span>,</span>}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>

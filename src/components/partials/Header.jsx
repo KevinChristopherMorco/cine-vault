d9fb0238ce1022a8a93b0e2490dd1a5809ccc1b0
Kevin Christopher Morco <christophermorco02@gmail.com>
2024-10-28 00:24:16 +0800

    adjusted search

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -6,9 +6,9 @@ import IconX from "@tabler/icons-react/dist/esm/icons/IconX.mjs";
 import IconBrightness2 from "@tabler/icons-react/dist/esm/icons/IconBrightness2.mjs";
 import IconMoonStars from "@tabler/icons-react/dist/esm/icons/IconMoonStars.mjs";
 
-import { useThemeContext } from "../../hooks/shared/ThemeProvider";
 import useMovieApi from "../../hooks/axios/useMovieApi";
 import useSearch from "../../hooks/search/useSearch";
+import useScreenResponsiveness from "../../hooks/shared/useScreenResponsiveness";
 import formatYear from "../../helpers/format/formatYear";
 
 const Header = () => {
@@ -20,7 +20,8 @@ const Header = () => {
   const { movieData, isLoading, handleSearchQueryEndpoint } = useMovieApi();
 
   useEffect(() => {
-    handleSearchQueryEndpoint(query);
+    const timeout = setTimeout(() => handleSearchQueryEndpoint(query), 150);
+    return () => clearTimeout(timeout);
   }, [query]);
 
   const handleInputChange = (event) => {
@@ -34,107 +35,183 @@ const Header = () => {
     }
   };
 
-  return (
-    <nav className="fixed z-[999] flex h-[4rem] w-full items-center justify-between border-b border-b-[var(--brand-color-600)] bg-[var(--bg-neutral)] p-4 lg:h-[4.5rem] dark:bg-[var(--brand-color-900)]">
-      {toggle ? (
-        <div className="w-full">
-          <div className="flex w-full items-center justify-between">
-            <input
-              type="text"
-              className="w-[90%] animate-fadeIn bg-transparent p-1 outline-none"
-              placeholder="Search for a movie"
-              onChange={handleInputChange}
-            />
-            <IconX className="animate-iconScale cursor-pointer" />
-          </div>
-          {view && (
-            <div className="fixed left-0 top-[4.5rem] h-screen w-full animate-fadeIn overflow-y-scroll bg-[var(--bg-neutral)] px-3 py-4 pb-[5rem]">
-              {!isLoading &&
-                movieData.map((search) => {
-                  const {
-                    data: {
-                      id,
-                      poster_path,
-                      title,
-                      release_date,
-                      credits: { cast },
-                    },
-                  } = search;
+  const {
+    screenSize: { sm, md, lg, xl, xxl },
+  } = useScreenResponsiveness();
 
-                  return (
-                    <Link
-                      to={`/movies-summary/${id}`}
-                      key={id}
-                      className="flex gap-4 border-b border-[var(--brand-color-700)] py-2 font-medium"
-                      onClick={() =>
-                        setSearch(() => ({ toggle: false, view: false }))
-                      }
-                    >
-                      <img
-                        src={
-                          poster_path
-                            ? `http://image.tmdb.org/t/p/w500${poster_path}`
-                            : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
-                        }
-                        alt=""
-                        className="min-h-[5rem] w-[15%] rounded-md"
-                      />
-                      <div>
-                        <p>{title}</p>
-                        <p className="text-[.8rem] text-gray-400">
-                          {formatYear(release_date)}
-                        </p>
-                        <div className="flex gap-1">
-                          {cast.slice(0, 2).map((cast, index) => (
+  return (
+    <>
+      {(sm || md) && (
+        <nav className="fixed z-[999] flex h-[4rem] w-full items-center justify-between border-b border-b-[var(--brand-color-600)] bg-[var(--bg-neutral)] p-4 lg:h-[4.5rem] dark:bg-[var(--brand-color-900)]">
+          {toggle ? (
+            <div className="w-full">
+              <div className="flex w-full items-center justify-between">
+                <input
+                  type="text"
+                  className="w-[90%] animate-fadeIn bg-transparent p-1 outline-none"
+                  placeholder="Search for a movie"
+                  onChange={handleInputChange}
+                />
+                <IconX className="animate-iconScale cursor-pointer" />
+              </div>
+              {view && (
+                <div className="fixed left-0 h-screen w-full animate-fadeIn overflow-y-scroll bg-[var(--bg-neutral)] px-3 py-4 pb-[5rem]">
+                  {!isLoading &&
+                    movieData.map((search) => {
+                      const {
+                        data: {
+                          id,
+                          poster_path,
+                          title,
+                          release_date,
+                          credits: { cast },
+                        },
+                      } = search;
+
+                      return (
+                        <Link
+                          to={`/movies-summary/${id}`}
+                          key={id}
+                          className="flex gap-4 border-b border-[var(--brand-color-700)] py-2 font-medium"
+                          onClick={() =>
+                            setSearch(() => ({ toggle: false, view: false }))
+                          }
+                        >
+                          <img
+                            src={
+                              poster_path
+                                ? `http://image.tmdb.org/t/p/w500${poster_path}`
+                                : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+                            }
+                            alt=""
+                            className="min-h-[5rem] w-[15%] rounded-md"
+                          />
+                          <div>
+                            <p>{title}</p>
                             <p className="text-[.8rem] text-gray-400">
-                              {cast.name}
-                              {index !== 1 && <span>,</span>}
+                              {formatYear(release_date)}
                             </p>
-                          ))}
-                        </div>
-                      </div>
-                    </Link>
-                  );
-                })}
+                            <div className="flex gap-1">
+                              {cast.slice(0, 2).map((cast, index) => (
+                                <p
+                                  key={index}
+                                  className="text-[.8rem] text-gray-400"
+                                >
+                                  {cast.name}
+                                  {index !== 1 && <span>,</span>}
+                                </p>
+                              ))}
+                            </div>
+                          </div>
+                        </Link>
+                      );
+                    })}
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="flex w-full items-center justify-between">
+              <div className="flex items-center gap-3">
+                <Link to={"/"} className="flex w-fit items-center font-bold">
+                  <p className="text-5xl text-[var(--brand-color-500)]">C</p>
+                  <p className="text-white">INEVAULT</p>
+                </Link>
+              </div>
+
+              <div className="flex gap-4">
+                <IconSearch
+                  className="h-6 w-6 animate-iconScale cursor-pointer"
+                  onClick={() =>
+                    setSearch((prev) => ({ ...prev, toggle: true }))
+                  }
+                />
+                <IconMenu2 className="h-6 w-6 animate-iconScale cursor-pointer" />
+              </div>
             </div>
           )}
-        </div>
-      ) : (
-        <div className="flex w-full items-center justify-between">
-          <div className="flex items-center gap-3">
-            <Link to={"/"} className="flex w-fit items-center font-bold">
-              <p className="text-5xl text-[var(--brand-color-500)]">C</p>
-              <p className="text-white">INEVAULT</p>
-            </Link>
-          </div>
-
-          <div className="flex gap-4">
-            <div>
-              <IconSearch
-                className="h-6 w-6 animate-iconScale cursor-pointer"
-                onClick={() => setSearch((prev) => ({ ...prev, toggle: true }))}
-              />
-            </div>
-            <div>
-              <IconMenu2 className="h-6 w-6 animate-iconScale cursor-pointer" />
-            </div>
-            {/* <div>
-              {theme === "light" ? (
-                <IconMoonStars
-                  className="h-6 w-6 animate-iconScale cursor-pointer transition-colors hover:text-yellow-500"
-                  onClick={() => handleChangeTheme()}
-                />
-              ) : (
-                <IconBrightness2
-                  className="h-6 w-6 animate-iconScale cursor-pointer transition-colors hover:text-yellow-500"
-                  onClick={() => handleChangeTheme()}
-                />
-              )}
-            </div> */}
-          </div>
-        </div>
+        </nav>
       )}
-    </nav>
+
+      {(lg || xl || xxl) && (
+        <nav className="fixed z-[999] flex h-[4rem] w-full items-center justify-between border-b border-b-[var(--brand-color-600)] bg-[var(--bg-neutral)] p-4 lg:h-[4.5rem] dark:bg-[var(--brand-color-900)]">
+          <div className="flex w-full items-center gap-6">
+            <div className="flex items-center gap-3">
+              <Link to={"/"} className="flex w-fit items-center font-bold">
+                <p className="text-5xl text-[var(--brand-color-500)]">C</p>
+                <p className="text-white">INEVAULT</p>
+              </Link>
+            </div>
+            <div className="flex gap-1">
+              <IconMenu2 className="h-6 w-6 animate-iconScale cursor-pointer" />
+              <p className="font-medium">Menu</p>
+            </div>
+            <div className="relative w-[50%]">
+              <input
+                type="text"
+                className="w-full animate-fadeIn rounded-md bg-[var(--neutral-hover)] bg-transparent p-2 outline-none"
+                placeholder="Search for a movie"
+                onChange={handleInputChange}
+              />
+
+              {view && (
+                <div className="absolute h-[85vh] w-full animate-fadeIn overflow-y-scroll bg-[var(--bg-neutral)] px-3 py-4 pb-[5rem]">
+                  {!isLoading &&
+                    movieData.map((search) => {
+                      const {
+                        data: {
+                          id,
+                          poster_path,
+                          title,
+                          release_date,
+                          credits: { cast },
+                        },
+                      } = search;
+
+                      return (
+                        <Link
+                          to={`/movies-summary/${id}`}
+                          key={id}
+                          className="flex gap-4 border-b border-[var(--brand-color-700)] py-2 font-medium"
+                          onClick={() =>
+                            setSearch(() => ({ toggle: false, view: false }))
+                          }
+                        >
+                          <img
+                            src={
+                              poster_path
+                                ? `http://image.tmdb.org/t/p/w500${poster_path}`
+                                : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+                            }
+                            alt=""
+                            className="min-h-[5rem] w-[15%] rounded-md"
+                          />
+                          <div>
+                            <p>{title}</p>
+                            <p className="text-[.8rem] text-gray-400">
+                              {formatYear(release_date)}
+                            </p>
+                            <div className="flex gap-1">
+                              {cast.slice(0, 2).map((cast, index) => (
+                                <p
+                                  key={index}
+                                  className="text-[.8rem] text-gray-400"
+                                >
+                                  {cast.name}
+                                  {index !== 1 && <span>,</span>}
+                                </p>
+                              ))}
+                            </div>
+                          </div>
+                        </Link>
+                      );
+                    })}
+                </div>
+              )}
+            </div>
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
